e047f89a09c7312f9e13544b8b487ef64a81fd33
Slopsie23 <sdebeer@gmail.com>
2025-11-08 00:45:13 +0100

    Update commanderdeckdoctor.py
    test svg

--- commanderdeckdoctor.py
+++ commanderdeckdoctor.py
@@ -51,7 +51,12 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
+def _post_process_ruling_with_svg(ruling_text):
+    # DIT IS EEN TEST:
+    test_html = "<h2>TEST: HTML WERKT!</h2>" + ruling_text.replace('{W}', '<span style="color:white; background-color: black;">[W]</span>')
+    return html.escape(test_html) 
+    # EINDE TEST
+    # ... de rest van uw oorspronkelijke code
 # ------------------ Logging ------------------
 logging.basicConfig(
     level=logging.WARNING,
